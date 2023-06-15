d34c81d1f4604d09263053d89f7f499b2cdaed8a
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-15 17:22:44 +0530

    [PALLAVI] Add: File

---
+++ NlogImplementaion/Program.cs
@@ -0,0 +1,32 @@
+﻿using NLog;
+
+namespace NlogImplementaion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+
+            try
+            {
+                logger.Info("Program started.");
+
+                // Your program logic here
+
+                logger.Info("Program completed successfully.");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "An error occurred in the program.");
+            }
+            finally
+            {
+                LogManager.Shutdown();
+            }
+
+            Console.ReadLine();
+        }
+    }
+    
+}
